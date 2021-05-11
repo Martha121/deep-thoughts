03b2a1f9f55432dc86590d4b18197a7fa52cfe29
MGcommits <marthagamez211@gmail.com>
2021-05-11 17:08:53 -0500

    Create React app

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const { authMiddleware } = require("./utils/auth");
 
 // import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require("./schemas");
 
-const db = require('./config/connection');
-
-
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -27,7 +26,16 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
